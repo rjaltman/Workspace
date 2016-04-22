cceda6fc3dc565cf025f1c77bf10553b1b54980a
Robert J. Altman <rjaltman04@gmail.com>
2016-04-22 15:58:50 -0500

    4222016: Added (older) TextRender Test for PS
    This was used to see what settings in the application manifest were
    necessary for proper rendering on high-DPI screens, since I was doing a
    lot of development at that time on screens that exceeded the industry
    standard, and it became frustrating to look at with improper text
    rendering (especially) in Snowshoes. This form is really, really simple,
    but I'm sure I'll revisit it.

---
+++ Indigo/TextRenderSolutions-PS/TextRenderSolutions-PS/Form1.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace TextRenderSolutions_PS
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Open Sans Light", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(-3, 119);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(578, 37);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "The quick brown fox jumps over the lazy doge.";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Open Sans Light", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(-3, 63);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(578, 37);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "The quick brown fox jumps over the lazy doge.";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Roboto", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(-2, 241);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(780, 44);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "The quick brown fox jumps over the lazy doge.";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.BackColor = System.Drawing.Color.DarkRed;
+            this.ClientSize = new System.Drawing.Size(571, 442);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label2);
+            this.DoubleBuffered = true;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
