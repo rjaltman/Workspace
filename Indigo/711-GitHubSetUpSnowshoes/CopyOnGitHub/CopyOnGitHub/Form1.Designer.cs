73e57b108f536e3803f2d2a34f87714cd02bdac4
Robert J. Altman <rjaltman04@gmail.com>
2016-07-11 17:49:37 -0500

    Snowshoes Installer for GitHub Repo (Incomplete)
    Start of project; this is just initiating the form and text. The actual
    copying will be tested soon (likely with a dump of files coming here as
    well).
    
    Sorry, I know, more IndigoBox / Pentelec stuff.

---
+++ Indigo/711-GitHubSetUpSnowshoes/CopyOnGitHub/CopyOnGitHub/Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace CopyOnGitHub
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.thanks = new System.Windows.Forms.Label();
+            this.subtext = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // thanks
+            // 
+            this.thanks.AutoSize = true;
+            this.thanks.Font = new System.Drawing.Font("Segoe UI Semibold", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.thanks.ForeColor = System.Drawing.Color.DeepSkyBlue;
+            this.thanks.Location = new System.Drawing.Point(167, 9);
+            this.thanks.Name = "thanks";
+            this.thanks.Size = new System.Drawing.Size(289, 37);
+            this.thanks.TabIndex = 0;
+            this.thanks.Text = "Thanks for evaluating!";
+            // 
+            // subtext
+            // 
+            this.subtext.Font = new System.Drawing.Font("Segoe UI", 12.25F);
+            this.subtext.ForeColor = System.Drawing.Color.Gray;
+            this.subtext.Location = new System.Drawing.Point(12, 59);
+            this.subtext.Name = "subtext";
+            this.subtext.Size = new System.Drawing.Size(599, 130);
+            this.subtext.TabIndex = 1;
+            this.subtext.Text = resources.GetString("subtext.Text");
+            this.subtext.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Transparent;
+            this.button1.FlatAppearance.BorderColor = System.Drawing.Color.DeepSkyBlue;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.ForeColor = System.Drawing.Color.DeepSkyBlue;
+            this.button1.Location = new System.Drawing.Point(236, 192);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(150, 26);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Get Set Up";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Gray;
+            this.label1.Location = new System.Drawing.Point(105, 241);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(412, 44);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Note: This process will also locally install Roboto, which is licensed under the " +
+    "Apache License 2.0 and can be found at https://github.com/google/roboto/.";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.ClientSize = new System.Drawing.Size(623, 339);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.subtext);
+            this.Controls.Add(this.thanks);
+            this.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label thanks;
+        private System.Windows.Forms.Label subtext;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
