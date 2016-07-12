2360437f77140f5bb3a99a4cc57cfb71f0c192a2
Robert J. Altman <rjaltman04@gmail.com>
2016-07-11 19:05:36 -0500

    Added Copying to Snowshoes GitHub Installer
    That is, basically gave the installer its main functionality, aside from
    being able to close itself.

--- Indigo/711-GitHubSetUpSnowshoes/CopyOnGitHub/CopyOnGitHub/Form1.Designer.cs
+++ Indigo/711-GitHubSetUpSnowshoes/CopyOnGitHub/CopyOnGitHub/Form1.Designer.cs
@@ -31,8 +31,11 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.thanks = new System.Windows.Forms.Label();
             this.subtext = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
-            this.label1 = new System.Windows.Forms.Label();
+            this.getSetUp = new System.Windows.Forms.Button();
+            this.robotoNotice = new System.Windows.Forms.Label();
+            this.getOutOfHere = new System.Windows.Forms.Label();
+            this.allDone = new System.Windows.Forms.Label();
+            this.closeButton = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // thanks
@@ -40,7 +43,7 @@
             this.thanks.AutoSize = true;
             this.thanks.Font = new System.Drawing.Font("Segoe UI Semibold", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.thanks.ForeColor = System.Drawing.Color.DeepSkyBlue;
-            this.thanks.Location = new System.Drawing.Point(167, 9);
+            this.thanks.Location = new System.Drawing.Point(167, 42);
             this.thanks.Name = "thanks";
             this.thanks.Size = new System.Drawing.Size(289, 37);
             this.thanks.TabIndex = 0;
@@ -50,52 +53,98 @@
             // 
             this.subtext.Font = new System.Drawing.Font("Segoe UI", 12.25F);
             this.subtext.ForeColor = System.Drawing.Color.Gray;
-            this.subtext.Location = new System.Drawing.Point(12, 59);
+            this.subtext.Location = new System.Drawing.Point(12, 92);
             this.subtext.Name = "subtext";
             this.subtext.Size = new System.Drawing.Size(599, 130);
             this.subtext.TabIndex = 1;
             this.subtext.Text = resources.GetString("subtext.Text");
             this.subtext.TextAlign = System.Drawing.ContentAlignment.TopCenter;
             // 
-            // button1
+            // getSetUp
             // 
-            this.button1.BackColor = System.Drawing.Color.Transparent;
-            this.button1.FlatAppearance.BorderColor = System.Drawing.Color.DeepSkyBlue;
-            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button1.ForeColor = System.Drawing.Color.DeepSkyBlue;
-            this.button1.Location = new System.Drawing.Point(236, 192);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(150, 26);
-            this.button1.TabIndex = 2;
-            this.button1.Text = "Get Set Up";
-            this.button1.UseVisualStyleBackColor = false;
+            this.getSetUp.BackColor = System.Drawing.Color.Transparent;
+            this.getSetUp.FlatAppearance.BorderColor = System.Drawing.Color.DeepSkyBlue;
+            this.getSetUp.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.getSetUp.ForeColor = System.Drawing.Color.DeepSkyBlue;
+            this.getSetUp.Location = new System.Drawing.Point(236, 225);
+            this.getSetUp.Name = "getSetUp";
+            this.getSetUp.Size = new System.Drawing.Size(150, 26);
+            this.getSetUp.TabIndex = 2;
+            this.getSetUp.Text = "Get Set Up";
+            this.getSetUp.UseVisualStyleBackColor = false;
+            this.getSetUp.Click += new System.EventHandler(this.getSetUp_Click);
             // 
-            // label1
+            // robotoNotice
             // 
-            this.label1.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.ForeColor = System.Drawing.Color.Gray;
-            this.label1.Location = new System.Drawing.Point(105, 241);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(412, 44);
-            this.label1.TabIndex = 3;
-            this.label1.Text = "Note: This process will also locally install Roboto, which is licensed under the " +
-    "Apache License 2.0 and can be found at https://github.com/google/roboto/.";
-            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.robotoNotice.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.robotoNotice.ForeColor = System.Drawing.Color.Silver;
+            this.robotoNotice.Location = new System.Drawing.Point(87, 274);
+            this.robotoNotice.Name = "robotoNotice";
+            this.robotoNotice.Size = new System.Drawing.Size(449, 44);
+            this.robotoNotice.TabIndex = 3;
+            this.robotoNotice.Text = "Note: This process will also locally install Roboto and Pacifico, the former of w" +
+    "hich is licensed under the Apache License 2.0 and latter under the SIL Open Font" +
+    " License 1.1.";
+            this.robotoNotice.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // getOutOfHere
+            // 
+            this.getOutOfHere.AutoSize = true;
+            this.getOutOfHere.Font = new System.Drawing.Font("Segoe UI", 10.25F);
+            this.getOutOfHere.ForeColor = System.Drawing.Color.Red;
+            this.getOutOfHere.Location = new System.Drawing.Point(594, 9);
+            this.getOutOfHere.Name = "getOutOfHere";
+            this.getOutOfHere.Size = new System.Drawing.Size(17, 19);
+            this.getOutOfHere.TabIndex = 4;
+            this.getOutOfHere.Text = "X";
+            this.getOutOfHere.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.getOutOfHere.Click += new System.EventHandler(this.getOutOfHere_Click);
+            // 
+            // allDone
+            // 
+            this.allDone.AutoSize = true;
+            this.allDone.Font = new System.Drawing.Font("Segoe UI Semibold", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.allDone.ForeColor = System.Drawing.Color.DeepSkyBlue;
+            this.allDone.Location = new System.Drawing.Point(111, 101);
+            this.allDone.Name = "allDone";
+            this.allDone.Size = new System.Drawing.Size(401, 37);
+            this.allDone.TabIndex = 5;
+            this.allDone.Text = "You can now close this window.";
+            this.allDone.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.allDone.Visible = false;
+            // 
+            // closeButton
+            // 
+            this.closeButton.BackColor = System.Drawing.Color.Transparent;
+            this.closeButton.FlatAppearance.BorderColor = System.Drawing.Color.DeepSkyBlue;
+            this.closeButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.closeButton.ForeColor = System.Drawing.Color.DeepSkyBlue;
+            this.closeButton.Location = new System.Drawing.Point(236, 163);
+            this.closeButton.Name = "closeButton";
+            this.closeButton.Size = new System.Drawing.Size(150, 26);
+            this.closeButton.TabIndex = 6;
+            this.closeButton.Text = "Close";
+            this.closeButton.UseVisualStyleBackColor = false;
+            this.closeButton.Visible = false;
+            this.closeButton.Click += new System.EventHandler(this.closeButton_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.WhiteSmoke;
-            this.ClientSize = new System.Drawing.Size(623, 339);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.button1);
+            this.ClientSize = new System.Drawing.Size(623, 326);
+            this.Controls.Add(this.closeButton);
+            this.Controls.Add(this.allDone);
+            this.Controls.Add(this.getOutOfHere);
+            this.Controls.Add(this.robotoNotice);
+            this.Controls.Add(this.getSetUp);
             this.Controls.Add(this.subtext);
             this.Controls.Add(this.thanks);
             this.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Snowshoes @ GitHub";
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -105,8 +154,11 @@
 
         private System.Windows.Forms.Label thanks;
         private System.Windows.Forms.Label subtext;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button getSetUp;
+        private System.Windows.Forms.Label robotoNotice;
+        private System.Windows.Forms.Label getOutOfHere;
+        private System.Windows.Forms.Label allDone;
+        private System.Windows.Forms.Button closeButton;
     }
 }
 
--- Indigo/711-GitHubSetUpSnowshoes/CopyOnGitHub/CopyOnGitHub/Form1.cs
+++ Indigo/711-GitHubSetUpSnowshoes/CopyOnGitHub/CopyOnGitHub/Form1.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,6 +17,111 @@ namespace CopyOnGitHub
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void getOutOfHere_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void copyThem()
+        {
+            //System.IO.FileInfo[] files = null;
+            //System.IO.DirectoryInfo[] subDirs = null;
+
+
+            //try
+            //{
+            //    files = initPath.GetFiles("*.*");
+            //}
+            //// MICROSOFT: "This is thrown if even one of the files requires permissions greater 
+            //// than the application provides."
+            //// Side question: Why did Microsoft create pseudo-folders to My Music, My Documents, and My Videos?
+
+            //catch (UnauthorizedAccessException e)
+            //{
+            //    // For now, we will not report any unauthorized access exceptions. We could log it, as in the Microsoft example, but...well, nahhh. 
+            //    // By that, this should be discussed with evaluators...and myself.
+            //}
+
+            //catch (System.IO.DirectoryNotFoundException e)
+            //{
+            //    Console.WriteLine(e.Message);
+            //}
+
+            //if (files != null)
+            //{
+            //    if (hiWhatNow == "Documents")
+            //    {
+            //        Directory.CreateDirectory(@"C:\ProjectSnowshoes\User\" + formalName + @"\Documents\" + initPath.FullName.Replace(docsCopyPath, ""));
+            //        arcadePanel_Reflektions.Text = "Creating Directory for User: " + @"\Documents\" + initPath.FullName.Replace(docsCopyPath, "");
+
+            //        foreach (System.IO.FileInfo fi in files)
+            //        {
+            //            File.Copy(fi.FullName, @"C:\ProjectSnowshoes\User\" + formalName + @"\Documents\" + fi.FullName.Replace(docsCopyPath, ""));
+            //            arcadePanel_Reflektions.Text = "Copying file: " + fi.FullName + " to Documents\\" + fi.FullName.Replace(docsCopyPath, "");
+            //            this.Invalidate();
+            //        }
+            //    }
+            //    else
+            //    {
+            //        Directory.CreateDirectory(@"C:\ProjectSnowshoes\User\" + formalName + @"\Pictures\" + initPath.FullName.Replace(imgCopyPath, ""));
+            //        arcadePanel_Reflektions.Text = "Creating Directory for User: " + @"\Pictures\" + initPath.FullName.Replace(imgCopyPath, "");
+
+            //        foreach (System.IO.FileInfo fi in files)
+            //        {
+            //            File.Copy(fi.FullName, @"C:\ProjectSnowshoes\User\" + formalName + @"\Pictures\" + fi.FullName.Replace(imgCopyPath, ""));
+            //            arcadePanel_Reflektions.Text = "Copying file: " + fi.FullName + " to Pictures\\" + fi.FullName.Replace(imgCopyPath, "");
+            //            this.Invalidate();
+            //        }
+            //    }
+            //    // "Now find all the subdirectories under this directory."
+            //    subDirs = initPath.GetDirectories();
+
+            //    foreach (System.IO.DirectoryInfo dirInfo in subDirs)
+            //    {
+            //        // "Recursive call for each subdirectory."
+            //        populateDirectories(dirInfo);
+            //    }
+
+
+
+            //}
+        }
+
+        private void getSetUp_Click(object sender, EventArgs e)
+        {
+
+            //Process.Start(@"ClientSide\System\Fonts\PSFontInstall.vbs");
+
+            ProcessStartInfo s = new ProcessStartInfo();
+            //s.CreateNoWindow = false;
+            s.UseShellExecute = false;
+            s.FileName = "xcopy";
+            //s.WindowStyle = ProcessWindowStyle.Hidden;
+            //Send the Source and destination as Arguments to the process
+            s.Arguments = @"ClientSide C:\ProjectSnowshoes\ /s /y";
+            Process.Start(s);
+
+            while (!File.Exists(@"C:\ProjectSnowshoes\System\Fonts\PSFontInstall.vbs"))
+            {
+                // pls
+            }
+            Process.Start(@"C:\ProjectSnowshoes\System\Fonts\PSFontInstall.vbs");
+            ////System.Diagnostics.Process.Start(@"xcopy ClientSide C:\ProjectSnowshoes\ /s");
+            //Process.Start(@"C:\ProjectSnowshoes\System\Fonts\PSFontInstall.vbs");
+
+            allDone.Visible = true;
+            closeButton.Visible = true;
+            robotoNotice.Visible = false;
+            subtext.Visible = false;
+            thanks.Visible = false;
+            getSetUp.Visible = false;
+        }
+
+        private void closeButton_Click(object sender, EventArgs e)
+        {
+            Close();
         }
     }
 }
